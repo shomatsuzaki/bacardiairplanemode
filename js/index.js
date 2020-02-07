679b027d6a2faadd4688f44981c7d7a5ea4cc000
Sho Matsuzaki <sho.g.matsuzaki@gmail.com>
2020-02-07 11:12:18 -0500

    Replaced setTimeout with setInterval

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
-	setTimeout(function() {
+	setInterval(function() {
 		if (navigator.online) {
 			$('section').html('There is a connection.');
 		}
 		else {
 			$('section').html("There is NOT a connection.");
 		}
+		console.log('testing...');
 	}, 1000);
 
 });
