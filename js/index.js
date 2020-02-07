634efaf08714763a5258f19486727b621a5cef2e
Sho Matsuzaki <sho.g.matsuzaki@gmail.com>
2020-02-07 11:16:50 -0500

    Fixed onLine typo

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
 
 	setInterval(function() {
-		if (navigator.online) {
+		if (navigator.onLine) {
 			$('section').html('There is a connection.');
 		}
 		else {
 			$('section').html("There is NOT a connection.");
 		}
-		console.log('testing...');
-	}, 1000);
+	}, 500);
 
 });
